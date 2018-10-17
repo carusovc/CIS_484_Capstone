4dff81a673fcca4c743a69a2c0ff62b83bea8af8
Anna Bui <buian@dukes.jmu.edu>
2018-10-17 13:48:07 -0400

    Program and Online form without functionality (linked to MasterPage.master)

--- CIS484Capstone/Online.aspx.cs
+++ CIS484Capstone/Online.aspx.cs
@@ -5,7 +5,7 @@ using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
 
-public partial class OnlineForm : System.Web.UI.Page
+public partial class Online : System.Web.UI.Page
 {
     protected void Page_Load(object sender, EventArgs e)
     {
