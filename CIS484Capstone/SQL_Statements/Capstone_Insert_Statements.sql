e4337e9a23cc147694429879b4125bee6d76a2b1
tylerstewie <44381301+tylerstewie@users.noreply.github.com>
2018-10-25 11:22:59 -0400

    Update Capstone_Insert_Statements.sql

--- CIS484Capstone/SQL_Statements/Capstone_Insert_Statements.sql
+++ CIS484Capstone/SQL_Statements/Capstone_Insert_Statements.sql
@@ -101,9 +101,143 @@ Insert into [dbo].[OnlineProgramType](OnlineProgramTypeName, LastUpdated, LastUp
 Insert into [dbo].[OnlineProgramType](OnlineProgramTypeName, LastUpdated, LastUpdatedBy) VALUES ('Skype: Opossums', GETDATE(), 'WildTekDevelopers');
 Insert into [dbo].[OnlineProgramType](OnlineProgramTypeName, LastUpdated, LastUpdatedBy) VALUES ('Skype: Snakes', GETDATE(), 'WildTekDevelopers');
                    
+INSERT INTO [dbo].[Program]([ProgramTypeID],[OrgID],[Status],[ProgramAddress],[CityCounty],[State],[OnOff],[NumberOfChildren],[NumberOfAdults],[PaymentNeeded],[ProgramDate],[ProgramTime],[EventMonth],[ExtraComments],[LastUpdated],[LastUpdatedBy])
+VALUES (1, 1, 'Completed', null, 'Blacksburg, Montgomery', 'Virginia', 'true', 0, 200, 'Y', '01/15/18', '1:00', 'January', 'Went well!', '10/25/2018', 'Tyler Stewart'); 
 
-INSERT INTO [dbo].[Program]([ProgramTypeID],[OrgID],[Status],[ProgramAddress],[CityCounty],[State],[OnOff],[NumberOfChildren],[NumberOfAdults],[PaymentNeeded],[ProgramDate],[ProgramTime],[EventMonth],[ExtraComments])
-VALUES (1, 1, 'Completed', null, 'Blacksburg, Montgomery', 'Virginia', 'true', 0, 200, 'Y', '1/15/18', '1:00', 'January', 'Was a great learning experience for all!');
+INSERT INTO [dbo].[Program]([ProgramTypeID],[OrgID],[Status],[ProgramAddress],[CityCounty],[State],[OnOff],[NumberOfChildren],[NumberOfAdults],[PaymentNeeded],[ProgramDate],[ProgramTime],[EventMonth],[ExtraComments],[LastUpdated],[LastUpdatedBy])
+VALUES (2, 5, 'Completed', null, 'Harrisonburg, Rockingham', 'Virginia', 'true', 200, 12, 'Y', '01/20/18', '3:00', 'January', 'Went well!', '10/25/2018', 'Tyler Stewart');
 
-                                      
+INSERT INTO [dbo].[Program]([ProgramTypeID],[OrgID],[Status],[ProgramAddress],[CityCounty],[State],[OnOff],[NumberOfChildren],[NumberOfAdults],[PaymentNeeded],[ProgramDate],[ProgramTime],[EventMonth],[ExtraComments],[LastUpdated],[LastUpdatedBy])
+VALUES (3, 7, 'Completed', null, 'Fishersville, Augusta', 'Virginia', 'false', 80, 3, 'N', '01/28/18', '5:00', 'January', 'Went ok!', '10/25/2018', 'Tyler Stewart');
+
+INSERT INTO [dbo].[Program]([ProgramTypeID],[OrgID],[Status],[ProgramAddress],[CityCounty],[State],[OnOff],[NumberOfChildren],[NumberOfAdults],[PaymentNeeded],[ProgramDate],[ProgramTime],[EventMonth],[ExtraComments],[LastUpdated],[LastUpdatedBy])
+VALUES (4, 15, 'Completed', null, 'Fishersville, Augusta', 'Virginia', 'true', 0, 40, 'N', '02/05/18', '4:00', 'February', 'Was a great group.', '10/25/2018', 'Tyler Stewart');
+
+INSERT INTO [dbo].[Program]([ProgramTypeID],[OrgID],[Status],[ProgramAddress],[CityCounty],[State],[OnOff],[NumberOfChildren],[NumberOfAdults],[PaymentNeeded],[ProgramDate],[ProgramTime],[EventMonth],[ExtraComments],[LastUpdated],[LastUpdatedBy])
+VALUES (5, 10, 'Completed', null, 'Waynesboro, Augusta', 'Virginia', 'false', 500, 15, 'Y', '02/20/18', '3:00', 'February', 'Was a great group.', '10/25/2018', 'Tyler Stewart');
+
+INSERT INTO [dbo].[Program]([ProgramTypeID],[OrgID],[Status],[ProgramAddress],[CityCounty],[State],[OnOff],[NumberOfChildren],[NumberOfAdults],[PaymentNeeded],[ProgramDate],[ProgramTime],[EventMonth],[ExtraComments],[LastUpdated],[LastUpdatedBy])
+VALUES (6, 8, 'Completed', null, 'Waynesboro, Augusta', 'Virginia', 'true', 250, 15, 'N', '03/14/18', '1:00', 'March', 'Was an awesome group!', '10/25/2018', 'Tyler Stewart');
+
+INSERT INTO [dbo].[Program]([ProgramTypeID],[OrgID],[Status],[ProgramAddress],[CityCounty],[State],[OnOff],[NumberOfChildren],[NumberOfAdults],[PaymentNeeded],[ProgramDate],[ProgramTime],[EventMonth],[ExtraComments],[LastUpdated],[LastUpdatedBy])
+VALUES (7, 2, 'Completed', null, 'Waynesboro, Augusta', 'Virginia', 'true', 40, 15, 'N', '03/25/18', '1:00', 'March', 'Was an ok group.', '10/25/2018', 'Tyler Stewart');
+
+INSERT INTO [dbo].[Program]([ProgramTypeID],[OrgID],[Status],[ProgramAddress],[CityCounty],[State],[OnOff],[NumberOfChildren],[NumberOfAdults],[PaymentNeeded],[ProgramDate],[ProgramTime],[EventMonth],[ExtraComments],[LastUpdated],[LastUpdatedBy])
+VALUES (8, 12, 'Completed', null, 'Bridgewater, Rockingham', 'Virginia', 'false', 250, 15, 'Y', '03/25/18', '3:00', 'March', 'Overall great experience!', '10/25/2018', 'Tyler Stewart');                                      
+ 
+INSERT INTO [dbo].[Program]([ProgramTypeID],[OrgID],[Status],[ProgramAddress],[CityCounty],[State],[OnOff],[NumberOfChildren],[NumberOfAdults],[PaymentNeeded],[ProgramDate],[ProgramTime],[EventMonth],[ExtraComments],[LastUpdated],[LastUpdatedBy])
+VALUES (9, 9, 'Completed', null, 'Dayton, Rockingham', 'Virginia', 'false', 200, 5, 'Y', '03/28/18', '3:00', 'March', 'Overall an ok experience.', '10/25/2018', 'Tyler Stewart');     
+
+INSERT INTO [dbo].[Program]([ProgramTypeID],[OrgID],[Status],[ProgramAddress],[CityCounty],[State],[OnOff],[NumberOfChildren],[NumberOfAdults],[PaymentNeeded],[ProgramDate],[ProgramTime],[EventMonth],[ExtraComments],[LastUpdated],[LastUpdatedBy])
+VALUES (10, 6, 'Completed', null, 'Dayton, Rockingham', 'Virginia', 'false', 25, 125, 'N', '04/15/18', '5:00', 'April', 'Great experience!', '10/25/2018', 'Tyler Stewart');                  
+
+INSERT INTO [dbo].[Program]([ProgramTypeID],[OrgID],[Status],[ProgramAddress],[CityCounty],[State],[OnOff],[NumberOfChildren],[NumberOfAdults],[PaymentNeeded],[ProgramDate],[ProgramTime],[EventMonth],[ExtraComments],[LastUpdated],[LastUpdatedBy])
+VALUES (11, 14, 'Completed', null, 'Dayton, Rockingham', 'Virginia', 'true', 17, 2, 'N', '04/20/18', '5:00', 'April', 'Great experience!', '10/25/2018', 'Tyler Stewart');      
+
+INSERT INTO [dbo].[Program]([ProgramTypeID],[OrgID],[Status],[ProgramAddress],[CityCounty],[State],[OnOff],[NumberOfChildren],[NumberOfAdults],[PaymentNeeded],[ProgramDate],[ProgramTime],[EventMonth],[ExtraComments],[LastUpdated],[LastUpdatedBy])
+VALUES (12, 3, 'Completed', null, 'Dayton, Rockingham', 'Virginia', 'false', 200, 9, 'Y', '05/09/18', '2:00', 'May', 'Alright group.', '10/25/2018', 'Tyler Stewart');      
+
+INSERT INTO [dbo].[Program]([ProgramTypeID],[OrgID],[Status],[ProgramAddress],[CityCounty],[State],[OnOff],[NumberOfChildren],[NumberOfAdults],[PaymentNeeded],[ProgramDate],[ProgramTime],[EventMonth],[ExtraComments],[LastUpdated],[LastUpdatedBy])
+VALUES (13, 4, 'Completed', null, 'Crozet, Albemarle', 'Virginia', 'true', 15, 10, 'N', '05/15/18', '5:00', 'May', 'What a time!', '10/25/2018', 'Tyler Stewart');           
+
+INSERT INTO [dbo].[Program]([ProgramTypeID],[OrgID],[Status],[ProgramAddress],[CityCounty],[State],[OnOff],[NumberOfChildren],[NumberOfAdults],[PaymentNeeded],[ProgramDate],[ProgramTime],[EventMonth],[ExtraComments],[LastUpdated],[LastUpdatedBy])
+VALUES (14, 13, 'Completed', null, 'Crozet, Albemarle', 'Virginia', 'true', 250, 15, 'N', '05/25/18', '5:00', 'May', 'Cannot wait for them to come again!', '10/25/2018', 'Tyler Stewart'); 
+
+INSERT INTO [dbo].[Program]([ProgramTypeID],[OrgID],[Status],[ProgramAddress],[CityCounty],[State],[OnOff],[NumberOfChildren],[NumberOfAdults],[PaymentNeeded],[ProgramDate],[ProgramTime],[EventMonth],[ExtraComments],[LastUpdated],[LastUpdatedBy])
+VALUES (15, 4, 'Completed', null, 'Crozet, Albemarle', 'Virginia', 'false', 115, 30, 'Y', '06/05/18', '6:00', 'June', 'What a time!', '10/25/2018', 'Tyler Stewart'); 
+
+INSERT INTO [dbo].[Program]([ProgramTypeID],[OrgID],[Status],[ProgramAddress],[CityCounty],[State],[OnOff],[NumberOfChildren],[NumberOfAdults],[PaymentNeeded],[ProgramDate],[ProgramTime],[EventMonth],[ExtraComments],[LastUpdated],[LastUpdatedBy])
+VALUES (16, 9, 'Completed', null, 'Quicksburg, Shenandoah', 'Virginia', 'false', 115, 30, 'Y', '06/08/18', '6:00', 'June', 'Tylers bday party!', '10/25/2018', 'Tyler Stewart'); 
+
+INSERT INTO [dbo].[Program]([ProgramTypeID],[OrgID],[Status],[ProgramAddress],[CityCounty],[State],[OnOff],[NumberOfChildren],[NumberOfAdults],[PaymentNeeded],[ProgramDate],[ProgramTime],[EventMonth],[ExtraComments],[LastUpdated],[LastUpdatedBy])
+VALUES (17, 11, 'Completed', null, 'Quicksburg, Shenandoah', 'Virginia', 'true', 130, 50, 'Y', '07/15/18', '3:00', 'July', 'Fun!', '10/25/2018', 'Tyler Stewart'); 
+
+INSERT INTO [dbo].[Program]([ProgramTypeID],[OrgID],[Status],[ProgramAddress],[CityCounty],[State],[OnOff],[NumberOfChildren],[NumberOfAdults],[PaymentNeeded],[ProgramDate],[ProgramTime],[EventMonth],[ExtraComments],[LastUpdated],[LastUpdatedBy])
+VALUES (18, 1, 'Completed', null, 'Shenandoah, Page', 'Virginia', 'true', 30, 50, 'N', '07/28/18', '4:00', 'July', 'Fun stuff!', '10/25/2018', 'Tyler Stewart'); 
+
+
+INSERT INTO [dbo].[Program]([ProgramTypeID],[OrgID],[Status],[ProgramAddress],[CityCounty],[State],[OnOff],[NumberOfChildren],[NumberOfAdults],[PaymentNeeded],[ProgramDate],[ProgramTime],[EventMonth],[ExtraComments],[LastUpdated],[LastUpdatedBy])
+VALUES (19, 8, 'Completed', null, 'Stuarts Draft, Augusta', 'Virginia', 'false', 330, 60, 'N', '08/20/18', '3:00', 'August', 'Alright group.', '10/25/2018', 'Tyler Stewart'); 
+
+INSERT INTO [dbo].[Program]([ProgramTypeID],[OrgID],[Status],[ProgramAddress],[CityCounty],[State],[OnOff],[NumberOfChildren],[NumberOfAdults],[PaymentNeeded],[ProgramDate],[ProgramTime],[EventMonth],[ExtraComments],[LastUpdated],[LastUpdatedBy])
+VALUES (20, 4, 'Completed', null, 'Stone Ridge, Loudoun', 'Virginia', 'true', 30, 50, 'Y', '09/25/18', '3:00', 'September', 'Great group.', '10/25/2018', 'Tyler Stewart');
+
+INSERT INTO [dbo].[Program]([ProgramTypeID],[OrgID],[Status],[ProgramAddress],[CityCounty],[State],[OnOff],[NumberOfChildren],[NumberOfAdults],[PaymentNeeded],[ProgramDate],[ProgramTime],[EventMonth],[ExtraComments],[LastUpdated],[LastUpdatedBy])
+VALUES (21, 12, 'Completed', null, 'Fredericksburg, Rappahannock', 'Virginia', 'false', 110, 50, 'Y', '10/15/18', '3:00', 'October', null, '10/25/2018', 'Tyler Stewart');
+
+INSERT INTO [dbo].[Program]([ProgramTypeID],[OrgID],[Status],[ProgramAddress],[CityCounty],[State],[OnOff],[NumberOfChildren],[NumberOfAdults],[PaymentNeeded],[ProgramDate],[ProgramTime],[EventMonth],[ExtraComments],[LastUpdated],[LastUpdatedBy])
+VALUES (22, 15, 'Completed', null, 'Stone Ridge, Loudoun', 'Virginia', 'true', 0, 150, 'Y', '10/25/18', '2:00', 'October', 'Okay group.', '10/25/2018', 'Tyler Stewart');
+
+Insert into dbo.ProgramAnimal(ProgramID,AnimalID,LastUpdated,LastUpdatedBy) VALUES (1, 14, '10/25/2018', 'Tyler Stewart');
+Insert into dbo.ProgramAnimal(ProgramID,AnimalID,LastUpdated,LastUpdatedBy) VALUES (1,15, '10/25/2018', 'Tyler Stewart');
+Insert into dbo.ProgramAnimal(ProgramID,AnimalID,LastUpdated,LastUpdatedBy) VALUES (1,8, '10/25/2018', 'Tyler Stewart');
+
+Insert into dbo.ProgramAnimal(ProgramID,AnimalID,LastUpdated,LastUpdatedBy) VALUES (2,5, '10/25/2018', 'Tyler Stewart');
+Insert into dbo.ProgramAnimal(ProgramID,AnimalID,LastUpdated,LastUpdatedBy) VALUES (2,18, '10/25/2018', 'Tyler Stewart');
+Insert into dbo.ProgramAnimal(ProgramID,AnimalID,LastUpdated,LastUpdatedBy) VALUES (2,17, '10/25/2018', 'Tyler Stewart');
+Insert into dbo.ProgramAnimal(ProgramID,AnimalID,LastUpdated,LastUpdatedBy) VALUES (2,22, '10/25/2018', 'Tyler Stewart');
+
+Insert into dbo.ProgramAnimal(ProgramID,AnimalID,LastUpdated,LastUpdatedBy) VALUES (3,3, '10/25/2018', 'Tyler Stewart');
+Insert into dbo.ProgramAnimal(ProgramID,AnimalID,LastUpdated,LastUpdatedBy) VALUES (3,8, '10/25/2018', 'Tyler Stewart');
+Insert into dbo.ProgramAnimal(ProgramID,AnimalID,LastUpdated,LastUpdatedBy) VALUES (3,9, '10/25/2018', 'Tyler Stewart');
+
+Insert into dbo.ProgramAnimal(ProgramID,AnimalID,LastUpdated,LastUpdatedBy) VALUES (4,2, '10/25/2018', 'Tyler Stewart');
+Insert into dbo.ProgramAnimal(ProgramID,AnimalID,LastUpdated,LastUpdatedBy) VALUES (4,12, '10/25/2018', 'Tyler Stewart');
+
+Insert into dbo.ProgramAnimal(ProgramID,AnimalID,LastUpdated,LastUpdatedBy) VALUES (5,1, '10/25/2018', 'Tyler Stewart');
+Insert into dbo.ProgramAnimal(ProgramID,AnimalID,LastUpdated,LastUpdatedBy) VALUES (5,2, '10/25/2018', 'Tyler Stewart');
+
+Insert into dbo.ProgramAnimal(ProgramID,AnimalID,LastUpdated,LastUpdatedBy) VALUES (6,3, '10/25/2018', 'Tyler Stewart');
+Insert into dbo.ProgramAnimal(ProgramID,AnimalID,LastUpdated,LastUpdatedBy) VALUES (6,7, '10/25/2018', 'Tyler Stewart');
+
+Insert into dbo.ProgramAnimal(ProgramID,AnimalID,LastUpdated,LastUpdatedBy) VALUES (7,5, '10/25/2018', 'Tyler Stewart');
+
+Insert into dbo.ProgramAnimal(ProgramID,AnimalID,LastUpdated,LastUpdatedBy) VALUES (8,6, '10/25/2018', 'Tyler Stewart');
+Insert into dbo.ProgramAnimal(ProgramID,AnimalID,LastUpdated,LastUpdatedBy) VALUES (8,9, '10/25/2018', 'Tyler Stewart');
+Insert into dbo.ProgramAnimal(ProgramID,AnimalID,LastUpdated,LastUpdatedBy) VALUES (8,10, '10/25/2018', 'Tyler Stewart');
+
+Insert into dbo.ProgramAnimal(ProgramID,AnimalID,LastUpdated,LastUpdatedBy) VALUES (9,11, '10/25/2018', 'Tyler Stewart');
+Insert into dbo.ProgramAnimal(ProgramID,AnimalID,LastUpdated,LastUpdatedBy) VALUES (9,15, '10/25/2018', 'Tyler Stewart');
+
+Insert into dbo.ProgramAnimal(ProgramID,AnimalID,LastUpdated,LastUpdatedBy) VALUES (10,16, '10/25/2018', 'Tyler Stewart');
+Insert into dbo.ProgramAnimal(ProgramID,AnimalID,LastUpdated,LastUpdatedBy) VALUES (10,17, '10/25/2018', 'Tyler Stewart');
+
+Insert into dbo.ProgramAnimal(ProgramID,AnimalID,LastUpdated,LastUpdatedBy) VALUES (11,4, '10/25/2018', 'Tyler Stewart');
+Insert into dbo.ProgramAnimal(ProgramID,AnimalID,LastUpdated,LastUpdatedBy) VALUES (11,14, '10/25/2018', 'Tyler Stewart');
+
+Insert into dbo.ProgramAnimal(ProgramID,AnimalID,LastUpdated,LastUpdatedBy) VALUES (12,18, '10/25/2018', 'Tyler Stewart');
+Insert into dbo.ProgramAnimal(ProgramID,AnimalID,LastUpdated,LastUpdatedBy) VALUES (12,7, '10/25/2018', 'Tyler Stewart');
+Insert into dbo.ProgramAnimal(ProgramID,AnimalID,LastUpdated,LastUpdatedBy) VALUES (12,11, '10/25/2018', 'Tyler Stewart');
+
+Insert into dbo.ProgramAnimal(ProgramID,AnimalID,LastUpdated,LastUpdatedBy) VALUES (13,19, '10/25/2018', 'Tyler Stewart');
+
+Insert into dbo.ProgramAnimal(ProgramID,AnimalID,LastUpdated,LastUpdatedBy) VALUES (14,20, '10/25/2018', 'Tyler Stewart');
+Insert into dbo.ProgramAnimal(ProgramID,AnimalID,LastUpdated,LastUpdatedBy) VALUES (14,18, '10/25/2018', 'Tyler Stewart');
+
+Insert into dbo.ProgramAnimal(ProgramID,AnimalID,LastUpdated,LastUpdatedBy) VALUES (15,21, '10/25/2018', 'Tyler Stewart');
+Insert into dbo.ProgramAnimal(ProgramID,AnimalID,LastUpdated,LastUpdatedBy) VALUES (15,1, '10/25/2018', 'Tyler Stewart');
+Insert into dbo.ProgramAnimal(ProgramID,AnimalID,LastUpdated,LastUpdatedBy) VALUES (15,15, '10/25/2018', 'Tyler Stewart');
+Insert into dbo.ProgramAnimal(ProgramID,AnimalID,LastUpdated,LastUpdatedBy) VALUES (15,2, '10/25/2018', 'Tyler Stewart');
+
+Insert into dbo.ProgramAnimal(ProgramID,AnimalID,LastUpdated,LastUpdatedBy) VALUES (16,12, '10/25/2018', 'Tyler Stewart');
+Insert into dbo.ProgramAnimal(ProgramID,AnimalID,LastUpdated,LastUpdatedBy) VALUES (16,22, '10/25/2018', 'Tyler Stewart');
+
+Insert into dbo.ProgramAnimal(ProgramID,AnimalID,LastUpdated,LastUpdatedBy) VALUES (17,1, '10/25/2018', 'Tyler Stewart');
+Insert into dbo.ProgramAnimal(ProgramID,AnimalID,LastUpdated,LastUpdatedBy) VALUES (17,3, '10/25/2018', 'Tyler Stewart');
+
+Insert into dbo.ProgramAnimal(ProgramID,AnimalID,LastUpdated,LastUpdatedBy) VALUES (18,18, '10/25/2018', 'Tyler Stewart');
+
+Insert into dbo.ProgramAnimal(ProgramID,AnimalID,LastUpdated,LastUpdatedBy) VALUES (18,4, '10/25/2018', 'Tyler Stewart');
+Insert into dbo.ProgramAnimal(ProgramID,AnimalID,LastUpdated,LastUpdatedBy) VALUES (10,12, '10/25/2018', 'Tyler Stewart');
+Insert into dbo.ProgramAnimal(ProgramID,AnimalID,LastUpdated,LastUpdatedBy) VALUES (12,9, '10/25/2018', 'Tyler Stewart');
+
+Insert into dbo.ProgramAnimal(ProgramID,AnimalID,LastUpdated,LastUpdatedBy) VALUES (10,16, '10/25/2018', 'Tyler Stewart');
+
+Insert into dbo.ProgramAnimal(ProgramID,AnimalID,LastUpdated,LastUpdatedBy) VALUES (15,7, '10/25/2018', 'Tyler Stewart');
+Insert into dbo.ProgramAnimal(ProgramID,AnimalID,LastUpdated,LastUpdatedBy) VALUES (1,9, '10/25/2018', 'Tyler Stewart');
+
+Insert into dbo.ProgramAnimal(ProgramID,AnimalID,LastUpdated,LastUpdatedBy) VALUES (5,21, '10/25/2018', 'Tyler Stewart');
+Insert into dbo.ProgramAnimal(ProgramID,AnimalID,LastUpdated,LastUpdatedBy) VALUES (6,17, '10/25/2018', 'Tyler Stewart');
+
+
                    
