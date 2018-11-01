2f93c09ba938852c0811ccd1f318f9499fc6fbd3
Megan Hovis <hovisme@dukes.jmu.edu>
2018-11-01 14:58:39 -0400

    navigation of the add edit view animals. animal tabels that need to be formatted by gunter.

---
+++ CIS484Capstone/AnimalPage.aspx.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class AnimalPage : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        addAnimal.Visible = false;
+    }
+
+    protected void btnAddAnimal_Click(object sender, EventArgs e)
+    {
+        addAnimal.Visible = true;
+        ViewAnimals.Visible = false;
+
+    }
+    protected void btnEditAnimal_Click(object sender, EventArgs e)
+    {
+        addAnimal.Visible = true;
+        ViewAnimals.Visible = false;
+
+    }
+    protected void btnViewAnimal_Click(object sender, EventArgs e)
+    {
+        addAnimal.Visible = false;
+        ViewAnimals.Visible = true;
+
+    }
+
+
+}
