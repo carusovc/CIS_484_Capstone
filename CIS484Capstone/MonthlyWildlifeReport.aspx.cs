e90111a622e3c2d49fba768f30e0fdc34450ad07
Megan Hovis <hovisme@dukes.jmu.edu>
2018-11-01 18:08:13 -0400

    Attempted editing

--- CIS484Capstone/MonthlyWildlifeReport.aspx.cs
+++ CIS484Capstone/MonthlyWildlifeReport.aspx.cs
@@ -4,7 +4,6 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
-using Excel = Microsoft.Office.Interop.Excel;
 using SQL = System.Data;
 
 public partial class MonthlyWildlifeReport : System.Web.UI.Page
