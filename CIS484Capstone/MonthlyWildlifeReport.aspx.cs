56bc9b5de43d306ae684921d9981304eab2dea20
Christian Caruso <carusocv@dukes.jmu.edu>
2018-11-01 14:25:44 -0400

    Final Update - CVC 11/1/18 2:25 PM 1. Deleted Unecessary Mircrosoft Protocol 2. Checked Sytem was working (AKA not crashing) 3. Not depolyable - dead links, not full functionality. 4. "Working" running prototype to be updated each day 5. This branch will be closed and merged into running master

--- CIS484Capstone/MonthlyWildlifeReport.aspx.cs
+++ CIS484Capstone/MonthlyWildlifeReport.aspx.cs
@@ -1,11 +1,6 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
-using Excel = Microsoft.Office.Interop.Excel;
-using SQL = System.Data;
 
 public partial class MonthlyWildlifeReport : System.Web.UI.Page
 {
